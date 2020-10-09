98f86d5f8df23db850b358010ca8661c7250e721
孙继峰 <sun.jifeng@outlook.com>
2020-10-09 09:49:31 +0800

    init

---
+++ src/test/java/tdd/training/TddTraningApplicationTests.java
@@ -0,0 +1,13 @@
+package tdd.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddTraningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
